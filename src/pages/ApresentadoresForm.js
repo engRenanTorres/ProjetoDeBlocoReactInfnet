17c1924af1feb6ad320bb21f272f413b17b75d98
Renan de Souza Torres <85042807+engRenanTorres@users.noreply.github.com>
2024-09-01 12:46:01 +0000

    aula 6 alinhando o useForm

--- src/pages/ApresentadoresForm.js
+++ src/pages/ApresentadoresForm.js
@@ -1,156 +1,107 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import "./apresentadoresForm.css";
 import { cepClient } from "../clients/cepClient.js";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import * as z from "zod";
+
+// Esquema de validação com zod
+const schema = z.object({
+  nome: z.string().min(3, "O nome deve ter pelo menos 3 caracteres"),
+  email: z.string().email("Email inválido"),
+  cep: z
+    .string()
+    .regex(
+      /^\d{8}|^\d{5}-\d{3}$/,
+      "O cep deve ser no formato 0000-000 ou apenas numeros"
+    ),
+  descricao: z.string().optional(),
+});
 
 export default function ApresentadoresForm() {
+  const {
+    register,
+    handleSubmit,
+    setValue,
+    setError,
+    formState: { errors },
+    watch,
+  } = useForm({
+    resolver: zodResolver(schema),
+  });
   const [rua, setRua] = useState("");
   const [cidade, setCidade] = useState("");
-  const [formData, setFormData] = useState({
-    nome: "",
-    email: "",
-    cep: "",
-    descricao: "",
-  });
-
-  const [erros, setErros] = useState({});
-
-  const handleChance = (e) => {
-    setFormData({
-      ...formData,
-      [e.target.name]: e.target.value,
-    });
-
-    setErros({
-      ...erros,
-      [e.target.name]: "",
-    });
-  };
-
-  const handleOnBlur = () => {
-    const newErros = validateForm();
-    if (Object.keys(newErros).length > 0) {
-      setErros(newErros);
-    }
-  };
 
   const handleBlurCep = async () => {
-    handleOnBlur();
-    const cep = formData.cep.replace("-", "");
-    try {
-      const data = await cepClient(cep);
-      console.log("data", data);
-      if (!data.erro) {
-        setRua(data.street);
-        setCidade(data.neighborhood);
-      } else {
-        alert("CEP não encontrado!");
+    const cep = watch("cep").replace("-", "");
+    if (cep.length === 8) {
+      try {
+        const data = await cepClient(cep);
+        if (data) {
+          setRua(data.street); // Ajuste para o nome correto do campo de retorno
+          setCidade(data.neighborhood); // Ajuste para o nome correto do campo de retorno
+        } else {
+          alert("CEP não encontrado!");
+        }
+      } catch (error) {
+        alert("Erro ao buscar CEP: " + error.message);
+        console.error(error);
       }
-    } catch (error) {
-      alert("Erro ao buscar CEP" + error.message);
-      console.error(error);
-    }
-  };
-
-  const validateForm = () => {
-    const newErros = {};
-
-    if (formData.nome.length < 3)
-      newErros.nome = "O nome deve ter pelo menos 3 caracteres";
-    if (!formData.email) newErros.email = "O email é obrigatório";
-    if (!formData.cep.match(/^\d{8}|^\d{5}-\d{3}$/))
-      newErros.cep = "O cep deve ser no formato 0000-000 ou apenas numeros";
-
-    return newErros;
-  };
-
-  useEffect(() => {});
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    const newErros = validateForm();
-    if (Object.keys(newErros).length > 0) {
-      setErros(newErros);
     } else {
-      console.log("Dados do formulario: ", formData);
-      setFormData({
-        nome: "",
-        email: "",
-        cep: "",
-        descricao: "",
-      });
+      alert("CEP inválido");
     }
+  };
+
+  const onSubmit = (e) => {
+    console.log("Dados do formulario: ", formData);
+    setFormData({
+      nome: "",
+      email: "",
+      cep: "",
+      descricao: "",
+    });
   };
 
   return (
     <div className="form-container">
       <h1>Inscrição Apresentadores</h1>
-      <form className="registration-form" onSubmit={handleSubmit}>
+      <form className="registration-form" onSubmit={handleSubmit(onSubmit)}>
         <div className="registration-input-list">
           <div className="input-group">
             <label htmlFor="nome">Nome</label>
-            <input
-              type="text"
-              id="nome"
-              name="nome"
-              onChange={handleChance}
-              onBlur={handleOnBlur}
-              value={formData.nome}
-            />
+            <input type="text" id="nome" {...register("nome")} />
+            {errors.nome && (
+              <span className="error">{errors.nome.message}</span>
+            )}
           </div>
-          {erros.nome && <span className="error">{erros.nome}</span>}
           <div className="input-group">
             <label htmlFor="email">E-mail</label>
-            <input
-              type="email"
-              id="email"
-              name="email"
-              onChange={handleChance}
-              value={formData.email}
-            />
+            <input type="email" id="email" {...register("email")} />
+            {errors.email && (
+              <span className="error">{errors.email.message}</span>
+            )}
           </div>
-          {erros.email && <span className="error">{erros.email}</span>}
           <div className="input-group">
             <label htmlFor="cep">CEP</label>
             <input
               type="text"
               id="cep"
-              name="cep"
-              onChange={handleChance}
+              {...register("cep")}
               onBlur={handleBlurCep}
-              value={formData.cep}
             />
+            {errors.cep && <span className="error">{errors.cep.message}</span>}
           </div>
-          {erros.cep && <span className="error">{erros.cep}</span>}
           <div className="input-group">
             <label htmlFor="descricao">Descrição</label>
-            <input
-              type="text"
-              id="descricao"
-              name="descricao"
-              onChange={handleChance}
-              value={formData.descricao}
-            />
+            <input type="text" id="descricao" {...register("descricao")} />
           </div>
-          {erros.descricao && <span className="error">{erros.descricao}</span>}
           <div className="input-group">
             <label htmlFor="rua">Rua</label>
-            <input
-              type="text"
-              id="rua"
-              name="rua"
-              onChange={handleChance}
-              value={rua}
-            />
+            <input type="text" id="rua" value={rua} readOnly />
           </div>
           <div className="input-group">
             <label htmlFor="cidade">Cidade</label>
-            <input
-              type="text"
-              id="cidade"
-              name="cidade"
-              onChange={handleChance}
-              value={cidade}
-            />
+            <input type="text" id="cidade" value={cidade} readOnly />
           </div>
         </div>
         <button className="form-button">Inserir</button>
